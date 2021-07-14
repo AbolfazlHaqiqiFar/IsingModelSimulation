60d4d13a0afc0c75677b1e502af1f1407ba4990c
Abolfazl Haqiqifar <afzhqq@gmail.com>
2021-07-15 00:09:40 +0430

    update Evolution function , add WriteTheLattice function and modify the code

--- medium.py
+++ medium.py
@@ -8,8 +8,9 @@ class Medium:
 
 ###########################################################################################
 
-    def __init__(self, num= 64, dim=1, initial_config= 'stochastic', \
-        initial_direction= 1, J=1, steps= 100, temp= 0):
+    def __init__(self, num= 64, dim=1, initial_config= 'stochastic', inputFilename="lattice.data",\
+                        initial_direction= 1, J=1, steps= 100, temp= 0,  display = False,\
+                                outputfilename="trajectory.xyz", dump_step = 100):
 
         """
         Medium class input include : 
@@ -23,7 +24,10 @@ class Medium:
         self.Steps = steps
         self.Temp = temp
         self.latticeSize = num
-        self.lattice = Lattice(n = num, d = dim, mode= initial_config, dirr= initial_direction, J = J)
+        self.output = outputfilename
+        self.dump_s = dump_step
+        self.lattice = Lattice(n = num, d = dim, mode = initial_config,\
+            inputfile = inputFilename, dirr = initial_direction, J = J, latticeDisplay = display)
         #print("ener: ", Lattice.energy())
         #print("polar: ", Lattice.polarization())
         #print(steps, temp)
@@ -59,23 +63,41 @@ class Medium:
 ###########################################################################################
 
     def Evolution(self, display=False):
-        
-        print("the relaxation time has been started. ")
+		# we run the system for 20% of the monte carlo steps as the relaxation time
+		# and won't insert it in our calculation
+        print("# The relaxation time has been started.")
         relaxation_steps = int(self.Steps / 5)
         for rs in range(relaxation_steps):
             self.MonteCarloSteps()
+		# ---------------------------------------------
+		# Now we start to calculate the energy and the magnerization of the system
+		# for the given Monte Carlos steps
+
+        print("# The relaxation time has been finished successfully.")
+        print("# The Evolution time has been started.")
+
+        polar_arr = []
+        energ_arr = []
 
         ave_totalE = 0
         ave_orderP = 0
         for s in range(self.Steps):
-	        totalE, orderP = self.MonteCarloSteps()
-                ave_totalE += totalE
-                ave_orderP += orderP
-                if display:
-                        print("%-4.d %-5.3f %-5.3f"%(s, totalE, orderP))
-                
+            totalE, orderP = self.MonteCarloSteps()
+            ave_totalE += totalE; ave_orderP += orderP
 
-                return (ave_totalE / self.Steps) ,(ave_orderP / self.Steps) 
+            polar_arr.append(orderP)
+            energ_arr.append(totalE)
+
+            polar_var = self.lattice.GetVariance(ave_orderP/self.Steps, polar_arr)
+            energ_var = self.lattice.GetVariance(ave_totalE/self.Steps, energ_arr)
+
+            if display:
+                print("%-4.d %-5.3f %-5.3f"%(s, totalE, orderP))
+
+            if self.dump_s != 0:
+                self.lattice.dumpXYZ(self.output, self.dump_s, self.Steps, s)
+
+        return (ave_totalE / self.Steps) , energ_var, (ave_orderP / self.Steps), polar_var
 
 
 ###########################################################################################
